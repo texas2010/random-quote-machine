1e5d2715b73f163720a0a6a0969e71f854890f86
texas2010 <9701609+texas2010@users.noreply.github.com>
2020-09-12 00:05:39 -0500

    add babel polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: ['@babel/polyfill', './src/index.js'],
   output: {
     path: path.resolve(__dirname, myPublicPath),
     filename: 'js/bundle.js'
